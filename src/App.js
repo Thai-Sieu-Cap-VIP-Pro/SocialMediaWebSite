9654d458c5533dd1846d094ca1971d2e6290dbf2
Ngô Gia Thái <81607294+Thai-Sieu-Cap-VIP-Pro@users.noreply.github.com>
2022-04-04 08:29:40 +0700

    Thai fix routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
 import Header from "./shareComponents/header/Header";
-import { Routes, Route, Redirect } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 import Auth from "./features/auth";
+import LoginForm from "./features/auth/components/LoginForm";
+import RegisterForm from "./features/auth/components/RegisterForm";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/auth" element={<Auth />} />
-        <Route path="/home" element={<Header />} />
+        <Route path="/" element={<Header />}></Route>
+        <Route path="/auth/*" element={<Auth />}></Route>
       </Routes>
+      <Outlet></Outlet>
     </div>
   );
 }
--- src/features/auth/index.js
+++ src/features/auth/index.js
@@ -1,18 +1,17 @@
 import React from "react";
-import { useMatch, Switch, Route, Routes } from "react-router-dom";
+import { Route, Routes, Outlet } from "react-router-dom";
 import NotFound from "../../shareComponents/notfound/NotFound";
-import Loginform from "./components/LoginForm";
+import LoginForm from "./components/LoginForm";
 import RegisterForm from "./components/RegisterForm";
 
 const Auth = () => {
-  const match = useMatch();
   return (
     <div>
       <Routes>
-        <Route path="login" element={Loginform} />
-        <Route path="register" element={RegisterForm} />
-        <Route component={NotFound} />
+        <Route path="login" element={<LoginForm />}></Route>
+        <Route path="register" element={<RegisterForm />}></Route>
       </Routes>
+      <Outlet />
     </div>
   );
 };
