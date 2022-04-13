a4549f9c307839ff9b6bb0fa7e561f21741c00cd
Ngô Gia Thái <81607294+Thai-Sieu-Cap-VIP-Pro@users.noreply.github.com>
2022-04-13 22:38:34 +0700

    Hoan thien code

--- src/features/chat/pages/ChatPage.js
+++ src/features/chat/pages/ChatPage.js
@@ -1,29 +1,32 @@
-import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
-import Header from '../../../shareComponents/header/Header';
-import ChatContent from '../components/ChatContent';
-import ListChat from '../components/ListChat';
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import Header from "../../../shareComponents/header/Header";
+import ChatContent from "../components/ChatContent";
+import ListChat from "../components/ListChat";
 
 const ChatPage = () => {
-    return (
-        <>
-            <Container fluid>
-                <Row>
-                    <Header></Header>
-                </Row>
-            </Container>
-            <Container style={{ marginTop: '15px' }}>
-                <Row>
-                    <Col md={3} style={{ paddingRight: 0, paddingLeft: 0 }}>
-                        <ListChat />
-                    </Col>
-                    <Col md={8} style={{ paddingRight: 0, paddingLeft: 0 }}>
-                        <ChatContent />
-                    </Col>
-                </Row>
-            </Container>
-        </>
-    );
+  return (
+    <>
+      <Container fluid>
+        <Row>
+          <Header></Header>
+        </Row>
+      </Container>
+      <Container style={{ marginTop: "100px" }}>
+        <Row>
+          <Col
+            md={{ span: 4, offset: 1 }}
+            style={{ paddingRight: 0, paddingLeft: 0 }}
+          >
+            <ListChat />
+          </Col>
+          <Col md={{ span: 6 }} style={{ paddingRight: 0, paddingLeft: 0 }}>
+            <ChatContent />
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 };
 
 export default ChatPage;
