c8df03ae384b0c2b7265167e835ba45efc7c5b07
HoangKhang0410 <khongnoihailan@gmail.com>
2022-05-16 20:40:41 +0700

    add unTym

--- src/api/ChatApi.js
+++ src/api/ChatApi.js
@@ -42,6 +42,10 @@ const ChatAPI = {
         const url = 'http://localhost:3001/api/chat/tymMessage';
         return axiosClient.patch(url, params);
     },
+    unTymMessage: (params) => {
+        const url = 'http://localhost:3001/api/chat/unTymMessage';
+        return axiosClient.patch(url, params);
+    },
 };
 
 export default ChatAPI;
