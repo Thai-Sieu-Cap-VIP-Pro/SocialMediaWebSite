e2374e1c3f2496597688705e47190568db4e84cf
Ngô Gia Thái <81607294+Thai-Sieu-Cap-VIP-Pro@users.noreply.github.com>
2022-05-23 15:20:41 +0700

    Thai

--- src/features/home/components/postHeader.js
+++ src/features/home/components/postHeader.js
@@ -6,9 +6,12 @@ import { faEllipsis } from "@fortawesome/free-solid-svg-icons";
 import { useDispatch } from "react-redux";
 import { ShowReportModal } from "../homeSlice";
 import ReportModal from "./reportModal";
+import { addActiveId } from "../../user/profileSlice";
+import { useNavigate } from "react-router-dom";
 
 const PostHeader = ({ postId, postUser }) => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   //show report modal
   const showModal = (id) => {
@@ -17,12 +20,18 @@ const PostHeader = ({ postId, postUser }) => {
     dispatch(action);
   };
 
+  const hanldeShowProfile = (id) => {
+    const action = addActiveId(id);
+    dispatch(action);
+    navigate("/account");
+  };
+
   return (
     <Row>
-      <Col md={1}>
+      <Col md={1} onClick={() => hanldeShowProfile(postUser._id)}>
         <img src={postUser?.avatar} alt="" />
       </Col>
-      <Col md={10}>
+      <Col md={10} onClick={() => hanldeShowProfile(postUser._id)}>
         <h6>{postUser?.name}</h6>
       </Col>
       <Col md={1}>
--- src/features/user/profileSlice.js
+++ src/features/user/profileSlice.js
@@ -1,55 +1,62 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import userAPI from '../../api/UserApi';
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import userAPI from "../../api/UserApi";
 
-export const getUserById = createAsyncThunk('user/getUserById', async (params) => {
-  console.log(params)
-  const userInfo = await userAPI.getUserInfo(params)
-  console.log(userInfo)
-  return userInfo
-})
+export const getUserById = createAsyncThunk(
+  "user/getUserById",
+  async (params) => {
+    console.log(params);
+    const userInfo = await userAPI.getUserInfo(params);
+    console.log(userInfo);
+    return userInfo;
+  }
+);
 
 export const updateUser = createAsyncThunk(
-  'user/updateUser',
+  "user/updateUser",
   async (params) => {
     const updatedUser = await userAPI.updateUser(params);
     return updatedUser;
   }
 );
 
-export const unFollow = createAsyncThunk('user/unFollow', async (params) => {
-  const unFollowUser = await userAPI.unFollow(params)
-  return unFollowUser
-})
+export const unFollow = createAsyncThunk("user/unFollow", async (params) => {
+  const unFollowUser = await userAPI.unFollow(params);
+  return unFollowUser;
+});
 
-export const getPostsByUserId = createAsyncThunk('user/getPostsByUserId', async (params) => {
-  console.log(params)
-  const posts = await userAPI.getPostsByUserId(params)
-  console.log(posts)
-  return posts
-})
+export const getPostsByUserId = createAsyncThunk(
+  "user/getPostsByUserId",
+  async (params) => {
+    console.log(params);
+    const posts = await userAPI.getPostsByUserId(params);
+    console.log(posts);
+    return posts;
+  }
+);
 
 const UserSlice = createSlice({
-  name: 'user',
+  name: "user",
   initialState: {
-    activeId: "62794b3efab692cd57d1428c",
+    activeId: "",
     userInfo: {},
     posts: [],
     isLoading: false,
   },
   reducers: {
     addActiveId: (state, action) => {
-      state.activeId = action.payload.id
-    }
+      console.log(action.payload);
+      state.activeId = action.payload;
+    },
   },
   extraReducers: {
     [getUserById.pending]: (state) => {
-      state.isLoading = true
+      state.isLoading = true;
     },
     [getUserById.fulfilled]: (state, action) => {
-      state.userInfo = action.payload.userInfo
+      state.userInfo = action.payload.userInfo;
     },
     [getUserById.rejected]: (state, action) => {
-      state.isLoading = false
+      state.isLoading = false;
     },
     [updateUser.pending]: (state) => {
       state.isLoading = true;
@@ -61,28 +68,28 @@ const UserSlice = createSlice({
       state.isLoading = false;
     },
     [unFollow.pending]: (state) => {
-      state.isLoading = true
+      state.isLoading = true;
     },
     [unFollow.fulfilled]: (state, action) => {
-      state.userInfo.following = action.payload.unfollowUser?.following
+      state.userInfo.following = action.payload.unfollowUser?.following;
     },
     [unFollow.rejected]: (state, action) => {
-      state.isLoading = false
+      state.isLoading = false;
     },
     [getPostsByUserId.pending]: (state) => {
-      state.isLoading = true
+      state.isLoading = true;
     },
     [getPostsByUserId.fulfilled]: (state, action) => {
-      state.posts = action.payload.listPost
+      state.posts = action.payload.listPost;
     },
     [getPostsByUserId.rejected]: (state, action) => {
-      state.isLoading = false
-    }
+      state.isLoading = false;
+    },
   },
 });
 
 const { reducer: userReducer, actions } = UserSlice;
 
-export const {} = actions;
+export const { addActiveId } = actions;
 
-export default userReducer
+export default userReducer;
