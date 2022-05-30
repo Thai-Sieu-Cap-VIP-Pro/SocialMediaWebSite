405d7744fac0e48cf4d12ada7a44da425976e40d
HoangKhang0410 <khongnoihailan@gmail.com>
2022-05-30 23:40:54 +0700

    fix update post

--- src/features/newpost/pages/NewPage.js
+++ src/features/newpost/pages/NewPage.js
@@ -10,59 +10,61 @@ import NewpostImage from '../components/newPostImage';
 import './newpage.scss';
 
 const NewPage = () => {
-  const dispatch = useDispatch();
-  const location = useLocation();
+    const dispatch = useDispatch();
+    const location = useLocation();
 
-  const post = location.state.post;
-  const postImages = post.images.map((item, index) => {
-    return item.split('.')[item.split('.').length - 1] === 'mp4'
-      ? {
-          url: item,
-          type: 'video',
-        }
-      : {
-          url: item,
-          type: 'image',
-        };
-  });
+    const post = location.state?.post;
+    const postImages = post?.images.map((item, index) => {
+        return item.split('.')[item.split('.').length - 1] === 'mp4'
+            ? {
+                  url: item,
+                  type: 'video',
+              }
+            : {
+                  url: item,
+                  type: 'image',
+              };
+    });
 
-  const [listImg, setlistImg] = useState(postImages || []);
-  const [valueInput, setValueInput] = useState(post?.content || '');
+    const [listImg, setlistImg] = useState(postImages || []);
+    const [valueInput, setValueInput] = useState(post?.content || '');
 
-  useEffect(async () => {
-    let action2 = getNotification();
-    await dispatch(action2).unwrap();
-  }, []);
+    useEffect(async () => {
+        let action2 = getNotification();
+        await dispatch(action2).unwrap();
+    }, []);
 
-  return (
-    <>
-      <Container fluid>
-        <Row>
-          <Header></Header>
-        </Row>
-      </Container>
-      <Container style={{ marginTop: '115px' }}>
-        <Row>
-          <Col md={{ span: 8, offset: 2 }} className="newWrapper">
-            <Row>
-              <NewpostHeader listImg={listImg} content={valueInput} />
-            </Row>
-            <Row>
-              <Col md={7} className="newImgWrapper">
-                <NewpostImage listImg={listImg} setlistImg={setlistImg} />
-              </Col>
-              <Col md={5}>
-                <NewpostContent
-                  valueInput={valueInput}
-                  setValueInput={setValueInput}
-                />
-              </Col>
-            </Row>
-          </Col>
-        </Row>
-      </Container>
-    </>
-  );
+    return (
+        <>
+            <Container fluid>
+                <Row>
+                    <Header></Header>
+                </Row>
+            </Container>
+            <Container style={{ marginTop: '115px' }}>
+                <Row>
+                    <Col md={{ span: 8, offset: 2 }} className="newWrapper">
+                        <Row>
+                            <NewpostHeader
+                                listImg={listImg}
+                                content={valueInput}
+                                isUpdate={post ? true : false}
+                                postId={post?._id}
+                            />
+                        </Row>
+                        <Row>
+                            <Col md={7} className="newImgWrapper">
+                                <NewpostImage listImg={listImg} setlistImg={setlistImg} />
+                            </Col>
+                            <Col md={5}>
+                                <NewpostContent valueInput={valueInput} setValueInput={setValueInput} />
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    );
 };
 
 export default NewPage;
